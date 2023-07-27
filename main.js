5461bb096acb683090866a91eedab86741c1148d
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-07-27 17:04:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -14,13 +14,11 @@ newCallBtn.addEventListener('click', function() {
 
     var formContainer = document.createElement('div');
     formContainer.className = 'form-container';
-    var formType = formContainer.dataset.type;
     formContainer.innerHTML = `
         <button type="button" class="call-type-btn">Customer</button>
         <button type="button" class="call-type-btn">MDU Customer</button>
         <button type="button" class="call-type-btn">Inquiry</button>
         <button type="button" class="call-type-btn">Other</button>
-        ${formTemplates['customer']}  <!-- Show the customer form by default -->
     `;
     document.getElementById('formsContainer').appendChild(formContainer);
 
@@ -31,40 +29,64 @@ newCallBtn.addEventListener('click', function() {
         });
     }
 
+    // Load the 'customer' form by default
+    changeCallType('customer', buttons[0]);
+
     formContainer.querySelector('.dismiss-btn').addEventListener('click', function() {
         formContainer.remove();
     });
+});
 
-    formContainer.querySelector('.callForm').addEventListener('submit', function(e) {
+function changeCallType(type, button) {
+    var formContainer = button.parentElement;
+
+    // Check if the requested form type exists
+    if (!formTemplates[type]) {
+        console.log("Form type '" + type + "' does not exist.");
+        return;
+    }
+
+    // Remove any existing form before appending a new one
+    var oldForm = formContainer.querySelector('.callForm');
+    if (oldForm) {
+        oldForm.remove();
+    }
+
+    // Append the new form
+    var newForm = document.createElement('form');
+    newForm.className = 'callForm';
+    newForm.dataset.type = type;
+    newForm.innerHTML = formTemplates[type];
+    formContainer.appendChild(newForm);
+
+    newForm.querySelector('.dismiss-btn').addEventListener('click', function() {
+        formContainer.remove();
+    });
+
+    newForm.addEventListener('submit', function(e) {
         e.preventDefault();
         var form = e.target;
-        var formType = form.dataset.type; // assuming you have added a 'type' dataset to the form
-        console.log("Form type: ", formType); // Debugging line
+        var formType = form.dataset.type;
+        console.log("Form type: ", formType);
         var formData = formHandlers[formType](form);
-        console.log("Form data: ", formData); // Debugging line
+        console.log("Form data: ", formData);
 
         if (formData === lastFormData) {
             alert('You have already submitted this information.');
             return;
         }
 
-        // Your code to display formData...
-        console.log("Displaying form data..."); // Debugging line
+        console.log("Displaying form data...");
         var infoContainer = document.getElementById('infoContainer');
         console.log(infoContainer);
         var infoItem = document.createElement('div');
         console.log(infoItem);
         infoItem.className = 'info-item';
         infoItem.textContent = formData;
-        console.log(infoItem); // Changed from console.log(info);
+        console.log(infoItem);
         infoContainer.appendChild(infoItem);
-        console.log("Form data displayed."); // Debugging line
+        console.log("Form data displayed.");
 
         lastFormData = formData;
     });
-});
-
-function changeCallType(type, button) {
-    var formContainer = button.parentElement;
-    formContainer.querySelector('.callForm').outerHTML = formTemplates[type]; // replace the form with the type of form template
 }
