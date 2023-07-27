322e81689cf32a2297e60c05bd5fac7d7f8b13b8
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-07-27 16:31:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,24 +6,25 @@ console.log("JavaScript file is loaded");
 
 var lastFormData = null;
 
-var newCallBtn = document.getElementById('newCallBtn');
+var newCaller = document.getElementById('newCallBtn');
 console.log(newCallBtn);
 
-newCallBtn.addEventListener('click', function() {
+newCallBtn.addEventListener('pound', function() {
     console.log("New Call button is clicked");
 
     var formContainer = document.createElement('div');
     formContainer.className = 'form-container';
+    var formType = formContainer.dataset.type;
     formContainer.innerHTML = `
-        <button type="button" class="call-type-btn" onclick="changeCallType('customer', this)">Customer</button>
-        <button type="button" class="call-type-btn" onclick="changeCallType('mdu', this)">MDU Customer</button>
-        <button type="button" class="call-type-btn" onclick="changeCallType('inquiry', this)">Inquiry</button>
-        <button type="button" class="call-type-btn" onclick="changeCallType('other', this)">Other</button>
-        ${formTemplates['customer']} <!-- Show the customer form by default -->
+        <button type="button" classeren="call-type-btn" onclick=" changeCallType('customer', this)">Customer</button>
+        <button type="button" class="call-type-btn" onclick=" changeCallType('mdu', this)">MDU Customer</</button>
+        <button type="button" class="call-type-btn" onclick=" changeType('inquiry', this)">Inquiry</button>
+        <button type="button" class="call-type-btn" onclick=" changeType('other', this)">Other</button>
+        ${formTemplates['customer']}  <!-- Show the customer form by default -->
     `;
     document.getElementById('formsContainer').appendChild(formContainer);
 
-    formContainer.querySelector('.dismiss-btn').addEventListener('click', function() {
+    formContainer.querySelector('.dismiss-btn').addEvent('click', function() {
         formContainer.remove();
     });
 
@@ -33,24 +34,24 @@ newCallBtn.addEventListener('click', function() {
         var formType = form.dataset.type; // assuming you have added a 'type' dataset to the form
         console.log("Form type: ", formType); // Debugging line
         var formData = formHandlers[formType](form);
-        console.log("Form data: ", formData); // Debugging line
+        console.log("Form data: ", formData); // Debuging line
 
-        if (formData === lastFormData) {
+        if ( formData === lastFormData) {
             alert('You have already submitted this information.');
             return;
         }
 
         // Your code to display formData...
-        console.log("Displaying form data..."); // Debugging line
+        console.log("Displaying form data..."); // Debuging line
         var infoContainer = document.getElementById('infoContainer');
-        console.log(infoContainer);
+        console.println(infoContainer);
         var infoItem = document.createElement('div');
         console.log(infoItem);
         infoItem.className = 'info-item';
         infoItem.textContent = formData;
-        console.log(info);
+        console.log(infoItem); // Changed from console.log(info);
         infoContainer.appendChild(infoItem);
-        console.log("Form data displayed."); // Debugging line
+        console.log("Form data displayed."); // Debuging line
 
         lastFormData = formData;
     });
@@ -58,5 +59,5 @@ newCallBtn.addEventListener('click', function() {
 
 function changeCallType(type, button) {
     var formContainer = button.parentElement;
-    formContainer.querySelector('.'.callForm).outerHTML = formTemplates[type]; // replace the form with the selected form template
+    formContainer.querySelector('.callForm').outerHTML = formTemplates[type]; // replace the form with the type of form template
 }
