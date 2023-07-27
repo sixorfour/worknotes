cb9522fc74b6db7087379c84a8453ed46c2db39a
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-07-27 15:51:57 -0600

    Update formConfig.js

--- formConfig.js
+++ formConfig.js
@@ -1,1 +1,28 @@
+// formConfig.js
+
+export const formTemplates = {
+    'customer': `
+        <form class="callForm" data-type="customer">
+            <input type="text" class="accountNumber" placeholder="Account Number" required> *required
+            ...
+        </form>
+    `,
+    'mdu': `
+        <form class callForm data-type="mdu">
+            <input type="text" class="hoaName" placeholder="HOA/MDU Name" required> *required
+            ...
+        </form>
+    `,
+    // Add more form templates for other call types
+};
+
+export const formHandlers = {
+    'customer': function(form) {
+        ...
+    },
+    'mdu': function(form) {
+        ...
+    },
+    // Add more form handlers here...
+};
 
