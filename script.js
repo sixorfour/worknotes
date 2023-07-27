b43e43964ecc84b4d5a9bf23cf3f1f81bab03fcd
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-07-27 01:27:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,100 @@
+var lastFormData = null;
+
 document.getElementById('newCallBtn').addEventListener('click', function() {
     var formContainer = document.createElement('div');
     formContainer.className = 'form-container';
     formContainer.innerHTML = `
-        <button class="call-type-btn" onclick="changeCallType('customer', this)">Customer</button>
-        <button class="call-type-btn" onclick="changeCallType('mdu', this)">MDU Customer</button>
-        <button class="call-type-btn" onclick="changeCallType('inquiry', this)">Inquiry</button>
-        <button class="call-type-btn" onclick="changeCallType('other', this)">Other</button>
-        <form>
-            <div class="customer-fields">
-                <label>Account Number*:</label><input type="text" class="account-number" required pattern="^[1-3][0-9]{3,4}$">
-                <label>Phone Number**:</label><input type="text" class="phone-number">
-                <label>Email Address**:</label><input type="text" class="email-address">
-            </div>
-            <div class="mdu-fields hidden">
-                <label>HOA/MDU Name*:</label><input type="text" class="hoa-name" required>
-                <label>SiteID/Address*:</label><input type="text" class="siteid-address" required>
-            </div>
-            <label>Customer Name*:</label><input type="text" class="customer-name" required>
-            <label>Notes*:</label><textarea class="notes" required></textarea>
+        <form class="callForm">
+            <input type="text" class="accountNumber" placeholder="Account Number" required> *required
+            <input type="text" class="hoaName hidden" placeholder="HOA/MDU Name" required> *required
+            <input type="text" class="siteId hidden" placeholder="SiteID/Address" required> *required
+            <button type="button" class="call-type-btn" onclick="changeCallType('customer', this)">Customer</button>
+            <button type="button" class="call-type-btn" onclick="changeCallType('mdu', this)">MDU Customer</button>
+            <button type="button" class="call-type-btn" onclick="changeCallType('inquiry', this)">Inquiry</button>
+            <button type="button" class="call-type-btn" onclick="changeCallType('other', this)">Other</button><br>
+            <input type="text" class="phoneNumber" placeholder="Phone Number"> **pick one<br>
+            <input type="text" class="customerName" placeholder="Customer Name" required> *required<br>
+            <input type="email" class="emailAddress" placeholder="Email Address"> **pick one<br>
+            <textarea class="notes" placeholder="Notes" required></textarea> *required<br>
             <button type="submit" class="submit-btn">Submit</button>
-            <button type="button" class="dismiss-btn" onclick="this.parentNode.parentNode.remove()">Dismiss</button>
+            <button type="button" class="dismiss-btn">Dismiss</button>
         </form>
-        <div class="error"></div>
     `;
     document.getElementById('formsContainer').appendChild(formContainer);
+
+    formContainer.querySelector('.dismiss-btn').addEventListener('click', function() {
+        formContainer.remove();
+    });
+
+    formContainer.querySelector('.callForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        var accountNumber = formContainer.querySelector('.accountNumber').value;
+        var phoneNumber = formContainer.querySelector('.phoneNumber').value;
+        var customerName = formContainer.querySelector('.customerName').value;
+        var emailAddress = formContainer.querySelector('.emailAddress').value;
+        var notes = formContainer.querySelector('.notes').value;
+        var hoaName = formContainer.querySelector('.hoaName').value;
+        var siteId = formContainer.querySelector('.siteId').value;
+
+        var currentFormData = accountNumber + phoneNumber + customerName + emailAddress + notes + hoaName + siteId;
+
+        if (currentFormData === lastFormData) {
+            alert('You have already submitted this information.');
+            return;
+        }
+
+        if (!accountNumberField.classList.contains('hidden') && !/^[123]\d{3,4}$/.test(accountNumber)) {
+            alert('Account number must be 4 or 5 digits long and start with 1, 2, or 3.');
+            return;
+        }
+
+        phoneNumber = phoneNumber.replace(/\D/g, '');
+        if (phoneNumber.startsWith('1')) {
+            phoneNumber = phoneNumber.substring(1);
+        }
+
+        if (phoneNumber.length !== 10) {
+            alert('Phone number must be 10 digits long and not start with 1.');
+            return;
+        }
+
+        if ((phoneNumber !== '' && emailAddress !== '') || (phoneNumber === '' && emailAddress === '')) {
+            alert('Please fill either Phone Number or Email Address, not both or none.');
+            return;
+        }
+
+        var infoContainer = document.getElementById('infoContainer');
+        var infoItem = document.createElement('div');
+        infoItem.className = 'info-item';
+        if (accountNumber !== '') {
+            infoItem.textContent = `${accountNumber}:${phoneNumber || emailAddress} ${customerName} -- ${notes}`;
+        } else {
+            infoItem.textContent = `MDU Customer:${phoneNumber || emailAddress} ${customerName} ${hoaName} ${siteId} -- ${notes}`;
+        }
+        infoContainer.appendChild(infoItem);
+        lastFormData = currentFormData;
+    });
 });
 
 function changeCallType(type, button) {
-    var form = button.parentNode.querySelector('form');
-    var customerFields = form.querySelector('.customer-fields');
-    var mduFields = form.querySelector('.mdu-fields');
-    if (type === 'customer') {
-        customerFields.classList.remove('hidden');
-        mduFields.classList.add('hidden');
-    } else if (type === 'mdu') {
-        customerFields.classList.add('hidden');
-        mduFields.classList.remove('hidden');
-    }
-    // Add similar conditions for 'inquiry' and 'other' when their fields are defined
-}
+    var formContainer = button.parentElement;
+    var accountNumberField = formContainer.querySelector('.accountNumber');
+    var hoaNameField = formContainer.querySelector('.hoaName');
+    var siteIdField = formContainer.querySelector('.siteId');
 
-document.addEventListener('submit', function(e) {
-    e.preventDefault();
-    var form = e.target;
-    var formData = new FormData(form);
-    var data = Object.fromEntries(formData);
-    if ((data['phone-number'] && data['email-address']) || (!data['phone-number'] && !data['email-address'])) {
-        form.parentNode.querySelector('.error').textContent = 'Please fill either Phone Number or Email Address, not both or none.';
-        return;
+    if (type === 'mdu') {
+        accountNumberField.classList.add('hidden');
+        hoaNameField.classList.remove('hidden');
+        siteIdField.classList.remove('hidden');
+        accountNumberField.removeAttribute('required');
+        hoaNameField.required = true;
+        siteIdField.required = true;
+    } else {
+        accountNumberField.classList.remove('hidden');
+        hoaNameField.classList.add('hidden');
+        siteIdField.classList.add('hidden');
+        accountNumberField.required = true;
+        hoaNameField.removeAttribute('required');
+        siteIdField.removeAttribute('required');
     }
-    var infoContainer = document.getElementById('infoContainer');
-    var infoItem = document.createElement('div');
-    infoItem.className = 'info-item';
-    var callType = form.parentNode.querySelector('.call-type-btn:not(.hidden)').textContent;
-    if (callType === 'Customer') {
-        infoItem.textContent = `${data['account-number']}:${data['phone-number'] || data['email-address']} ${data['customer-name']} -- ${data['notes']}`;
-    } else if (callType === 'MDU Customer') {
-        infoItem.textContent = `MDU Customer:${data['phone-number'] || data['email-address']} ${data['customer-name']} ${data['hoa-name']} ${data['siteid-address']} -- ${data['notes']}`;
-    }
-    // Add similar conditions for 'Inquiry' and 'Other' when their display format is defined
-    infoContainer.appendChild(infoItem);
-});
+}
