549794e243d10235918b8c1bec68503bc72ae46d
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-07-27 20:57:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,114 @@
+// main.js
 
+import { formTemplates, formHandlers } from './formConfig.js';
+
+console.log("JavaScript file is loaded");
+
+var lastFormData = null;
+
+var newCallBtn = document.getElementById('newCallBtn');
+console.log(newCallBtn);
+
+newCallBtn.addEventListener('click', function() {
+    console.log("New Call button is clicked");
+
+    var formContainer = document.createElement('div');
+    formContainer.className = 'form-container';
+    formContainer.innerHTML = `
+        <div class="call-type-btn-container">
+            <button type="button" class="call-type-btn">Customer</button>
+            <button type="button" class="call-type-btn">MDU Customer</button>
+            <button type="button" class="call-type-btn">Inquiry</button>
+            <button type="button" class="call-type-btn">Other</button>
+        </div>
+        ${formTemplates['customer']}  <!-- Show the customer form by default -->
+    `;
+    document.getElementById('formsContainer').appendChild(formContainer);
+
+    var callTypeButtons = formContainer.querySelectorAll('.call-type-btn');
+    callTypeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            changeCallType(button.textContent.toLowerCase(), button);
+        });
+    });
+
+    var dismissButton = formContainer.querySelector('.dismiss-btn');
+    dismissButton.addEventListener('click', function() {
+        formContainer.remove();
+    });
+
+    var callForm = formContainer.querySelector('.callForm');
+    addSubmitListener(callForm);
+});
+
+function addSubmitListener(form) {
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        var formType = form.dataset.type; // assuming you have added a 'type' dataset to the form
+        console.log("Form type: ", formType); // Debugging line
+        var formData = formHandlers[formType](form);
+        console.log("Form data: ", formData); // Debugging line
+
+        if ( formData === lastFormData) {
+            alert('You have already submitted this information.');
+            return;
+        }
+
+        // Your code to display formData...
+        console.log("Displaying form data..."); // Debugging line
+        var infoContainer = document.getElementById('infoContainer');
+        console.log(infoContainer);
+        var infoItem = document.createElement('div');
+        console.log(infoItem);
+        infoItem.className = 'info-item';
+        infoItem.textContent = formData;
+        console.log(infoItem); // Changed from console.log(info);
+        infoContainer.appendChild(infoItem);
+        console.log("Form data displayed."); // Debugging line
+
+        lastFormData = formData;
+    });
+}
+
+function changeCallType(type, button) {
+    var formContainer = button.parentElement.parentElement;
+
+    // Check if the requested form type exists
+    if (!formTemplates[type]) {
+        console.log("Form type '" + type + "' does not exist.");
+        return;
+    }
+
+    // Store the values from the old form
+    var oldForm = formContainer.querySelector('.callForm');
+    var oldFormData = {};
+    if (oldForm) {
+        var oldInputs = oldForm.querySelectorAll('input, textarea');
+        oldInputs.forEach(function(input) {
+            oldFormData[input.className] = input.value;
+        });
+        oldForm.remove();
+    }
+
+    // Append the new form
+    var newForm = document.createElement('form');
+    newForm.innerHTML = formTemplates[type];
+    newForm.className = 'callForm';
+    newForm.dataset.type = type; // Set the 'type' dataset to the new form type
+    formContainer.appendChild(newForm);
+
+    // Populate the fields in the new form with the old form data, if available
+    var newInputs = newForm.querySelectorAll('input, textarea');
+    newInputs.forEach(function(input) {
+        if (oldFormData.hasOwnProperty(input.className)) {
+            input.value = oldFormData[input.className];
+        }
+    });
+
+    var dismissButton = newForm.querySelector('.dismiss-btn');
+    dismissButton.addEventListener('click', function() {
+        formContainer.remove();
+    });
+
+    addSubmitListener(newForm);
+}
