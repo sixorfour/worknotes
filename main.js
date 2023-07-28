4a5ab77d31903f813f1aaef43394cde503b6284d
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-07-27 18:09:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,25 +15,44 @@ newCallBtn.addEventListener('click', function() {
     var formContainer = document.createElement('div');
     formContainer.className = 'form-container';
     formContainer.innerHTML = `
-        <button type="button" class="call-type-btn">Customer</button>
-        <button type="button" class="call-type-btn">MDU Customer</button>
-        <button type="button" class="call-type-btn">Inquiry</button>
-        <button type="button" class="call-type-btn">Other</button>
+        <button type="button" class="call-type-btn" onclick="changeCallType('customer', this)">Customer</button>
+        <button type="button" class="call-type-btn" onclick="changeCallType('mdu customer', this)">MDU Customer</button>
+        <button type="button" class="call-type-btn" onclick="changeCallType('inquiry', this)">Inquiry</button>
+        <button type="button" class="call-type-btn" onclick="changeCallType('other', this)">Other</button>
+        ${formTemplates['customer']}  <!-- Show the customer form by default -->
     `;
     document.getElementById('formsContainer').appendChild(formContainer);
 
-    var buttons = formContainer.getElementsByClassName('call-type-btn');
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(e) {
-            changeCallType(e.target.textContent.toLowerCase(), e.target);
-        });
-    }
-
-    // Load the 'customer' form by default
-    changeCallType('customer', buttons[0]);
-
     formContainer.querySelector('.dismiss-btn').addEventListener('click', function() {
         formContainer.remove();
+    });
+
+    formContainer.querySelector('.callForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        var form = e.target;
+        var formType = form.dataset.type; // assuming you have added a 'type' dataset to the form
+        console.log("Form type: ", formType); // Debugging line
+        var formData = formHandlers[formType](form);
+        console.log("Form data: ", formData); // Debuging line
+
+        if ( formData === lastFormData) {
+            alert('You have already submitted this information.');
+            return;
+        }
+
+        // Your code to display formData...
+        console.log("Displaying form data..."); // Debuging line
+        var infoContainer = document.getElementById('infoContainer');
+        console.log(infoContainer);
+        var infoItem = document.createElement('div');
+        console.log(infoItem);
+        infoItem.className = 'info-item';
+        infoItem.textContent = formData;
+        console.log(infoItem); // Changed from console.log(info);
+        infoContainer.appendChild(infoItem);
+        console.log("Form data displayed."); // Debuging line
+
+        lastFormData = formData;
     });
 });
 
@@ -46,9 +65,14 @@ function changeCallType(type, button) {
         return;
     }
 
-    // Remove any existing form before appending a new one
+    // Store the values from the old form
     var oldForm = formContainer.querySelector('.callForm');
+    var oldFormData = {};
     if (oldForm) {
+        var oldInputs = oldForm.querySelectorAll('input, textarea');
+        oldInputs.forEach(function(input) {
+            oldFormData[input.className] = input.value;
+        });
         oldForm.remove();
     }
 
@@ -59,34 +83,11 @@ function changeCallType(type, button) {
     newForm.innerHTML = formTemplates[type];
     formContainer.appendChild(newForm);
 
-    newForm.querySelector('.dismiss-btn').addEventListener('click', function() {
-        formContainer.remove();
-    });
-
-    newForm.addEventListener('submit', function(e) {
-        e.preventDefault();
-        var form = e.target;
-        var formType = form.dataset.type;
-        console.log("Form type: ", formType);
-        var formData = formHandlers[formType](form);
-        console.log("Form data: ", formData);
-
-        if (formData === lastFormData) {
-            alert('You have already submitted this information.');
-            return;
+    // Populate the fields in the new form with the old form data, if available
+    var newInputs = newForm.querySelectorAll('input, textarea');
+    newInputs.forEach(function(input) {
+        if (oldFormData.hasOwnProperty(input.className)) {
+            input.value = oldFormData[input.className];
         }
-
-        console.log("Displaying form data...");
-        var infoContainer = document.getElementById('infoContainer');
-        console.log(infoContainer);
-        var infoItem = document.createElement('div');
-        console.log(infoItem);
-        infoItem.className = 'info-item';
-        infoItem.textContent = formData;
-        console.log(infoItem);
-        infoContainer.appendChild(infoItem);
-        console.log("Form data displayed.");
-
-        lastFormData = formData;
     });
 }
